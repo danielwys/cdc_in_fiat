404e34456cd6d619f73d24555d334574be8dd553
dawo5010 <me@dawo.me>
2020-10-22 18:37:47 +0800

    Cleaned up code, used Promise instead of callback.

--- contentScript.js
+++ contentScript.js
@@ -1,33 +1,42 @@
 let elements = document.getElementsByClassName("card-name d-block text-blue-300");
 
-console.log(elements);
-let url = 'https://api.coingecko.com/api/v3/simple/price?ids=crypto-com-chain&vs_currencies=usd'
+let croUrl = 'https://api.coingecko.com/api/v3/simple/price?ids=crypto-com-chain&vs_currencies=usd';
 
-fetch(url).then(response => response.text()).then(text => modifyPrice(parsePrice(text))).catch(error => console.log(error));
+function getCroPrice(callback) {
+    return fetch(croUrl).then(response => response.text());
+}
 
 function parsePrice(text) {
     let priceObject = JSON.parse(text);
-    return priceObject["crypto-com-chain"].usd;
+    return Object.values(priceObject)[0].usd;
 }
 
-function modifyPrice(price) {
-    console.log(price)
+Promise.all(
+    [
+        getCroPrice(),
+    ]
+).then(response => {
+    modifyPrice(response);
+})
+
+function modifyPrice(croPriceObj) {
+    let croPrice = parsePrice(croPriceObj);
+    
     for (item in elements) {
         switch (elements[item].innerHTML) {
             case "1,000,000 CRO":
-                elements[item].innerHTML = (price * 1000000).toFixed(2) + " USD"
+                elements[item].innerHTML = (1000000 * croPrice).toFixed(2) + " USD"
                 break;
             case "100,000 CRO":
-                elements[item].innerHTML = (price * 100000).toFixed(2) + " USD"
+                elements[item].innerHTML = (100000 * croPrice).toFixed(2) + " USD"
                 break;
             case "10,000 CRO":
-                elements[item].innerHTML = (price * 10000).toFixed(2) + " USD"
+                elements[item].innerHTML = (10000 * croPrice).toFixed(2) + " USD"
                 break;
             case "1,000 CRO":
-                elements[item].innerHTML = (price * 1000).toFixed(2) + " USD"
+                elements[item].innerHTML = (1000 * croPrice).toFixed(2) + " USD"
                 break;
         }
 
-        console.log(elements[item].innerHTML)
     }
 }
